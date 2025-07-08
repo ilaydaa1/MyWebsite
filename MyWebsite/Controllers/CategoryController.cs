3f6f25c2cb2e57754cc59fcf0311b3a4d4f2e9be
İlaydaa1 <2100004517@stu.iku.edu.tr>
2025-07-09 00:08:24 +0300

    Add project files.

---
+++ MyWebsite/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyWebsite.Data;
+using MyWebsite.Models;
+
+namespace MyWebsite.Controllers
+{
+    //pass the data to see in the table when run the project
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name ==obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match te Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);//to add category
+                _db.SaveChanges();//go to the db and create that category
+                return RedirectToAction("Index");
+            }
+            return View();
+            
+        }
+    }
+}
---
+++ MyWebsite/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyWebsite.Models;
+namespace MyWebsite.Data
+  
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options) 
+
+        {
+            
+        }
+        //create table which name is Categories in DB
+        public DbSet<Category> Categories { get; set; }
+
+        //make seed data
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "History", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "SciFi", DisplayOrder = 3 }
+                );
+        }
+    }
+}
